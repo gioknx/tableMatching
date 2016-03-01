35a2a36ae2bfe5927a4eb2a1a68464d6007e0ec8
gioknx <giovanikx@gmail.com>
2016-03-01 17:24:38 -0300

    Fixed script

--- script.rb
+++ script.rb
@@ -3,33 +3,33 @@
 
 require 'csv'
 
-input_csv_path = "ALL-DOMAINS-BR-ATUALIZADO.csv"
+input_csv_path = "lista_entrada.csv"
 output_csv_path = "lista_comparacao.csv"
 
-dominios_antigos = []
-dominios_novos = []
+produtos_antigos = []
+produtos_novos = []
 
 nova_lista = []
 
 
-## Pegamos as listas de dominios e colocamo-nas em arrays
+## Pegamos as listas de produtos e colocamo-nas em arrays
 CSV.foreach(input_csv_path) do |row|
-	dominios_antigos << row[0]
-	dominios_novos << row[1]
+	produtos_antigos << row[0]
+	produtos_novos << row[1]
 end
 
 ## Checamos a correspondencia
-dominios_antigos.each do |domain|
-	# Se a lista antiga incluir o dominio, colocaremo-os lado a lado
-	if dominios_novos.include? domain
-		nova_lista << [ domain, domain ]
+produtos_antigos.each do |product|
+	# Se a lista antiga incluir o produto, colocaremo-os lado a lado
+	if produtos_novos.include? product
+		nova_lista << [ product, product ]
 	# Se não, colocaremos ele ao lado de um espaço em branco
 	else
-		nova_lista << [domain, ""]
+		nova_lista << [product, ""]
 	end
 end
 
-
+## Escrevemos as duas colunas no arquivo de saída
 CSV.open(output_csv_path, "wb") do |csv|
   	nova_lista.each do |row|
 		csv << [row[0], row[1]]
